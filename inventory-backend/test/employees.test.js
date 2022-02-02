c182b8740cba350b80dabf301ad8f8ab6498ebc1
Oscar Elhanafey <oelhanafey@rocksolidhq.com>
2022-02-02 16:57:19 -0500

    started backend tests

---
+++ inventory-backend/test/employees.test.js
@@ -0,0 +1,8 @@
+describe("testing employees model", () => {
+    beforeAll(() => {
+        // Initialize database to base values
+    });
+    test("adds a new employee", () => {});
+
+    test("removes an existing employee", () => {});
+});
---
+++ inventory-backend/test/items.test.js
@@ -0,0 +1,9 @@
+describe("testing items model", () => {
+    beforeAll(() => {
+        // Initizalise base database values
+    });
+
+    test("adding an item", () => {});
+
+    test("removing an item", () => {});
+});
